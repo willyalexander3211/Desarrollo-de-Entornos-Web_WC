4221f8dfdb59250ae4c4df41c87aeb5fde586b0a
willyalexander3211 <63274400+willyalexander3211@users.noreply.github.com>
2020-04-18 19:23:26 -0500

    Add files via upload

---
+++ 2351 - Desarrollo de Entornos Web - T2AA/DEW_Semana_02/Laboratorio-Semana02/js/codigos-indice.js
@@ -0,0 +1,14 @@
+        var titu = document.getElementById("h1banner");
+        titu.innerHTML = "Todo Pesca";
+        titu.style.color = "red";
+        titu.style.textAlign = "center";
+        
+        var im = document.getElementById("imgbaner");
+        im.style.width = "100%";
+        im.style.height = "150px";
+        
+        var f = document.getElementById("cal");
+        f.innerHTML = "Fecha: 12/04/2020";
+        f.style.color = "red";
+
+document.getElementsByClassName("parrafo").innerHTML = "La pesca se define como aquella actividad que se realiza para extraer peces. Puede realizarse en aguas continentales o marítimas. Ancestralmente, la pesca ha consistido en una de las actividades económicas más tempranas de muchos pueblos del mundo.";
