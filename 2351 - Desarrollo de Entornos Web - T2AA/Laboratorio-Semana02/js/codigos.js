58fa00257562209c30ccd4aff23530fb9931f3d5
willyalexander3211 <63274400+willyalexander3211@users.noreply.github.com>
2020-04-18 19:19:25 -0500

    Add files via upload

---
+++ 2351 - Desarrollo de Entornos Web - T2AA/Laboratorio-Semana02/js/codigos.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+document.getElementById("h1banner").innerHTML = "Todo Pesca";
+lementById("h1banner").
+var titu = document.getElementById("h1banner");
+        titu.innerHTML = "Todo Pesca";
+        titu.style.color = "red";
+        titu.style.textAlign = "center";
+        
+        var im = document.getElementById("imgbaner");
+        im.style.width = "100%";
+        im.style.height = "150px";
+        
+        var f = document.getElementById("cal");
+        f.innerHTML = "Fecha: 12/04/2020";
+        f.style.color = "red";
